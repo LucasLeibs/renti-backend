acd9043a776cbe86eb5ed98582c43b7243a9a942
LucasLeibs <dr.lucasleibs@gmail.com>
2021-01-11 13:11:36 -0800

    got rid of rental table, do not think its necessary

--- db/migrate/20210108230335_create_reviews.rb
+++ db/migrate/20210108230335_create_reviews.rb
@@ -2,7 +2,11 @@ class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :rental, null: false, foreign_key: true
+      t.string :address
+      t.string :city
+      t.string :state 
+      t.integer :zipcode
+
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(name: 'Lucas')
+review1 = Review.create(user: user1, address: '565 Lukas St', city: "Seattle", state: "WA", zipcode: "98260")
+
+puts "seeded"
